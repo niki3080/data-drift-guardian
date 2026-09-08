e8f4bff935ff116f731b34c7e23b4488dcf6548d
akaStark <levbulanov2013@yandex.ru>
2026-09-08 18:26:24 +0500

    feat(registry): add registry fabric for metric functions

---
+++ metrics/registry.py
@@ -0,0 +1,23 @@
+from config.parse_config import Metric
+
+from typing import Protocol
+
+import pandas as pd
+
+class MetricFn(Protocol):
+    def __call__(self, 
+                 reference: pd.Series, 
+                 current: pd.Series, 
+                 **kwargs) -> float : ...
+
+
+METRIC_REGISTRY: dict[Metric, MetricFn] = {}
+
+
+def register(metric: Metric):
+    def wrapper(fn: MetricFn):
+        METRIC_REGISTRY[metric] = fn
+        return fn
+    return wrapper
+
+
