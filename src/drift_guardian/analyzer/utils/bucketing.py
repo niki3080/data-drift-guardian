89798dac2830b538f624414ffcb9cde8280c9ebb
akaStark <levbulanov2013@yandex.ru>
2026-09-19 16:27:48 +0500

    fix(make_counts): fix possible type error in sorting

--- src/drift_guardian/analyzer/utils/bucketing.py
+++ src/drift_guardian/analyzer/utils/bucketing.py
@@ -1,58 +1,64 @@
 from src.drift_guardian.schema.models import CategoricalRef, NumericRef
 
-import pandas as pd
 import numpy as np
+import pandas as pd
 
+OTHER_BUCKET = "other"
 
 def make_counts(reference: CategoricalRef | NumericRef, current: pd.Series):
-    if reference['type'] == 'categorical' or reference['low_cardinality']:
-        ref_buckets = reference['categories'].copy()
+    feature_type = reference["type"]
 
-        other_bucket_cats = set(
-            reference['merge_info']['other_bucket']['categories']
-        )
-        reference_cats = set(ref_buckets.keys())
+    if feature_type == "numeric" and not reference.get("low_cardinality", False):
+        bins = reference["decile_bins"]["deciles"]
+        ref_counts = reference["decile_bins"]["frequencies"]
 
-        normal_cats = reference_cats - other_bucket_cats
+        cur_counts, _ = np.histogram(current.dropna(), bins=bins)
 
-        cur_value_counts = current.value_counts()
+        return ref_counts, cur_counts
 
-        cur_buckets = cur_value_counts[
-            cur_value_counts.index.isin(normal_cats)
-        ].to_dict()
+    if feature_type == "categorical" or (
+        feature_type == "numeric" and reference.get("low_cardinality", False)
+    ):
+        categories = reference["categories"]
 
-        other_mask = (
-            cur_value_counts.index.isin(other_bucket_cats)
-            | ~cur_value_counts.index.isin(reference_cats)
-        )
+        merge_info = reference.get("merge_info") or {}
+        other_bucket = merge_info.get("other_bucket") or {}
 
-        if other_bucket_cats or (~cur_value_counts.index.isin(reference_cats)).any():
-            cur_buckets['other'] = int(cur_value_counts[other_mask].sum())
+        merged_categories = set(other_bucket.get("categories") or [])
+        other_ref_count = other_bucket.get("merge_cats_sum", 0)
 
-        if other_bucket_cats:
-            ref_buckets['other'] = reference['merge_info']['other_bucket']['merge_cats_sum']
+        current_counts = current.value_counts(dropna=True).to_dict()
 
-        ref_buckets = {
-            k: v for k, v in ref_buckets.items()
-            if k not in other_bucket_cats
-        }
+        ref_counts = []
+        cur_counts = []
 
-        for key in ref_buckets:
-            cur_buckets.setdefault(key, 0)
+        known_non_merged_categories = []
 
-        ref_buckets = dict(sorted(ref_buckets.items()))
-        cur_buckets = dict(sorted(cur_buckets.items()))
+        for category, ref_count in categories.items():
+            if category in merged_categories:
+                continue
 
-        ref_counts = list(ref_buckets.values())
-        cur_counts = list(cur_buckets.values())
+            known_non_merged_categories.append(category)
 
-    elif reference['type'] == 'numeric':
-        ref_counts = reference['decile_bins']['frequencies']
+            ref_counts.append(ref_count)
+            cur_counts.append(current_counts.get(category, 0))
 
-        deciles = reference['decile_bins']['deciles']
-        cur_counts, _ = np.histogram(current.dropna(), deciles)
+        known_categories = set(categories.keys())
 
-    else:
-        raise ValueError(f"Unknown feature type in reference: {reference['type']}")
+        other_current_count = 0
 
-    return ref_counts, cur_counts
+        for category, count in current_counts.items():
+            if category in merged_categories:
+                other_current_count += count
+            elif category not in known_categories:
+                other_current_count += count
+
+        has_other_bucket = bool(merged_categories) or other_ref_count > 0 or other_current_count > 0
+
+        if has_other_bucket:
+            ref_counts.append(other_ref_count)
+            cur_counts.append(other_current_count)
+
+        return ref_counts, cur_counts
+
+    raise ValueError(f"Unknown feature type in reference: {feature_type}")
